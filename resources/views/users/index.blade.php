aa2dc32e70ff243a103bda7d11fa841b7165fb6a
Mateus Figueredo <mateusfigueredo037@gmail.com>
2022-07-08 17:28:12 -0300

    aula:6 template blade

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,37 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listagem de Usuarios</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div>
-        <h1>Listagem de Usuarios</h1>
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Data Cadastro</th>
-                <th scope="col">Ações</th>
-              </tr>
-            </thead>
-            @foreach ($users as $user)
-            <tbody>
-              <tr>
-                <th scope="row">{{$user->id}}</th>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>{{date('d/m/Y - H:i',strtotime($user->created_at))}}</td>
-                <td><a class="btn btn-info text-white" href="{{route('users.show', $user->id)}}">Visualizar</a></td>
-              </tr>
-            </tbody>
-            @endforeach
-          </table>
-    </div>
-</body>
-</html>
+@extends('template.user')
+@section('title', 'Listagem de usuários')
+@section('body')
+<h1>Listagem de Usuarios</h1>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Email</th>
+        <th scope="col">Data Cadastro</th>
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
+    @foreach ($users as $user)
+    <tbody>
+      <tr>
+        <th scope="row">{{$user->id}}</th>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{date('d/m/Y - H:i',strtotime($user->created_at))}}</td>
+        <td><a class="btn btn-info text-white" href="{{route('users.show', $user->id)}}">Visualizar</a></td>
+      </tr>
+    </tbody>
+    @endforeach
+  </table>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,40 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Usuario: {{$user->name}}</title>
-</head>
-<body>
-    <div>
-        <h1>Usuario: {{$user->name}}</h1>
-        <table class="table">
-            <thead class="text-center">
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Data Cadastro</th>
-                <th scope="col">Ações</th>
-              </tr>
-            </thead>
-            @foreach ($users as $user)
-            <tbody class="text-center">
-              <tr>
-                <th scope="row">{{$user->id}}</th>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>{{date('d/m/Y - H:i',strtotime($user->created_at))}}</td>
-                <td>
-                    <a class="btn btn-warning text-white" href="">Editar</a>
-                    <a class="btn btn-danger text-white" href="">Deletar</a>
-                </td>
-              </tr>
-            </tbody>
-            @endforeach
-          </table>
-    </div>
-</body>
-</html>
+@extends('template.user')
+@section('title', 'Visualizar Usuário')
+@section('body')
+<h1>Usuario: {{$user->name}}</h1>
+<table class="table">
+  <thead class="text-center">
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Email</th>
+        <th scope="col">Data Cadastro</th>
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
+    @foreach ($users as $user)
+    <tbody class="text-center">
+      <tr>
+        <th scope="row">{{$user->id}}</th>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{date('d/m/Y - H:i',strtotime($user->created_at))}}</td>
+        <td>
+            <a class="btn btn-warning text-white" href="">Editar</a>
+            <a class="btn btn-danger text-white" href="">Deletar</a>
+        </td>
+      </tr>
+    </tbody>
+    @endforeach
+  </table>    
+@endsection
